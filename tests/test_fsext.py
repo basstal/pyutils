8ef78d46e19423a880b368193cf4c169d0663015
basstal <330475004@qq.com>
2022-07-14 13:19:58 +0800

    修正测试用例路径问题

--- tests/test_fsext.py
+++ tests/test_fsext.py
@@ -10,6 +10,10 @@ class TestShortHand(unittest.TestCase):
             os.chdir('tests')
         self.assertEqual(fs.to_base64('abs'), '')
         self.assertNotEqual(fs.to_base64('./data/to_base64.png'), '')
-        content = fs.to_base64('./data/to_base64.png', './output/to_base64')
-        with open('./output/to_base64', 'r+', encoding='utf-8') as f:
+        output_dir = './output'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_file = os.path.join(output_dir, 'to_base64')
+        content = fs.to_base64('./data/to_base64.png', output_file)
+        with open(output_file, 'r+', encoding='utf-8') as f:
             self.assertEqual(f.readline(), content)
